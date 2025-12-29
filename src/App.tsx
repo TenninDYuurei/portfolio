962d6213e1a4836ee9c7f7efce73c9d72fab8600
Tennin <divinejovakporaye@gmail.com>
2025-12-29 13:45:57 +0100

    initialized

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+const App: React.FC = () => {
+  return (
+    <main>
+      <div className="w-screen h-screen text-primary-gold bg-dark-black">
+apple pie
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        "primary-black":"#19171b",
+        "secondary-black":"#2f2921",
+        "primary-gold":"#9e8123",
+        "secondary-gold":"#563a17",
+        "dark-black":"#252628",
+      }
+    },
+  },
+  plugins: [],
+};
+
+export default config;
