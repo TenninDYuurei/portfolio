884446bd753340c7b544c2225f3e6b713d07011d
Tennin <divinejovakporaye@gmail.com>
2025-12-30 16:37:45 +0100

    changes

--- src/components/secondSection/SecondSection.tsx
+++ src/components/secondSection/SecondSection.tsx
@@ -52,7 +52,7 @@ const SecondSection = () => {
                     key={currentInfo}
                     initial={{ opacity: 0, y: 20, scale: 0.995 }}
                     animate={{ opacity: 1, y: 0, scale: 1 }}
-                    transition={{ duration: 0.35, ease: "linear" }}
+                    transition={{ duration: 0.35, ease: "easeOut" }}
                 >
                     {
                         currentInfo === 'bio' && (
